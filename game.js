e0db429385cc9de0c857aea852efa38c4c7c8eb7
Noah Finer <nfiner0149@gmail.com>
2015-06-05 19:48:45 -0600

    Add basic levels

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 //Initalizing variables
-var amtOfCols = 20;
-var amtOfRows = 10; // should be amtOfCols/2 for squares
+var lvl = l1;
+
+var lvlselect = false;
+
+var amtOfCols = lvl.cols;
+var amtOfRows = lvl.cols/2; // should be amtOfCols/2 for squares
 
 var maxX = amtOfCols - 1;
 var maxY = amtOfRows - 1;
@@ -8,17 +12,48 @@ var maxY = amtOfRows - 1;
 var cellHeight, cellWidth;
 var moveHeight, moveWidth;
 
-var amtOfEnemies = 4;
-
 var ec = [];
 var es = [];
 
+var amtOfEnemies = lvl.ecords.length;
+
 var ws = [];
-var wc = [[2, 5], [3, 5], [4, 5], [5, 5], [5, 6], [5, 7], [5, 8]];
+var wc = lvl.wcords;
+
+var pc = lvl.pcords;
 
 var pInit = false;
 
+var speed = 1;
+
 var gameActive = false;
+
+var initVars = function() {
+  //Initalizing variables
+  lvlselect = false;
+
+  amtOfCols = lvl.cols;
+  amtOfRows = lvl.cols/2; // should be amtOfCols/2 for squares
+
+  maxX = amtOfCols - 1
+  maxY = amtOfRows - 1;
+
+  ec = [];
+  es = [];
+
+  amtOfEnemies = lvl.ecords.length;
+
+  ws = [];
+  wc = lvl.wcords;
+
+  pc = lvl.pcords;
+
+  pInit = false;
+
+  speed = 1;
+
+  gameActive = false;
+};
 
 var initsizes = function() {
   var innerWindowHeight = $(window).height;
@@ -53,7 +88,7 @@ var Player = function() {
   this.x = 0;
   this.y = 0;
   this.html = "<div id='player' style='transition: margin-left 0.1s, margin-top 0.1s ease-in-out; -webkit-transition: margin-left 0.1s, margin-top 0.1s ease-in-out'><div id='player-mask' class='background-full'></div></div>"
-  this.speed = 1;
+  this.speed = speed;
   this.speed1 = this.speed + 1;
   this.id = '#player';
   this.type = 'player';
@@ -101,6 +136,9 @@ var Player = function() {
     }
     this.x -= dist;
     this.update();
+    if(enemyCheck(this.x, this.y)) {
+      this.die();
+    }
   }
   this.right = function(dist) {
     if(this.x + dist >= maxX) {
@@ -112,6 +150,9 @@ var Player = function() {
     }
     this.x += dist;
     this.update();
+    if(enemyCheck(this.x, this.y)) {
+      this.die();
+    }
   }
   this.up = function(dist) {
     if(this.y - dist <= 0) {
@@ -123,6 +164,9 @@ var Player = function() {
     }
     this.y -= dist;
     this.update();
+    if(enemyCheck(this.x, this.y)) {
+      this.die();
+    }
   }
   this.down = function(dist) {
     if(this.y + dist >= maxY) {
@@ -134,7 +178,23 @@ var Player = function() {
     }
     this.y += dist;
     this.update();
+    if(enemyCheck(this.x, this.y)) {
+      this.die();
+    }
   }
+}
+
+var enemyCheck = function(x, y) {
+  var response = false;
+  es.forEach(function(item) {
+    item.getDirection(x, y);
+    if(item.num === es.length - 1) {
+      if(checkForEnemy(x, y)) {
+        response = true;
+      }
+    }
+  });
+  return response;
 }
 
 
@@ -341,27 +401,39 @@ var deleteGame = function() {
   $('#playbutton').html('click to play!');
 }
 
+var hi = 1;
+
 var initGame = function() {
+  initVars();
+  lvlselect = false;
   $('.enemy').remove();
-  $('.wall').css('margin', '0px');
-  for(i = 0; i < ws.length; i++) {
-    ws[i].update();
+  $('.wall').remove();
+  ws = [];
+  for(i = 0; i < wc.length; i++) {
+    ws[i] = new Wall(wc[i][0], wc[i][1], i);
   }
   gameActive = true;
   initsizes();
   drawGrid(amtOfCols, amtOfRows);
   updateVals();
+  $('#player').remove();
+  delete p;
   p = new Player();
-  p.place(1, 1);
+  p.place(lvl.pcords[0], lvl.pcords[1]);
+  es = [];
+  // for(i = 0; i < amtOfEnemies; i++) {
+  //   es[i] = new Enemy(i);
+  //   var randX = Math.floor(Math.random()*amtOfCols);
+  //   var randY = Math.floor(Math.random()*amtOfRows);
+  //   // while(checkForEnemy(randX, randY)) { // avoids enemies spawning on each other
+  //   //   var randX = Math.floor(Math.random()*amtOfCols);
+  //   //   var randY = Math.floor(Math.random()*amtOfRows);
+  //   // };
+  //   es[i].place(randX, randY);
+  // }
   for(i = 0; i < amtOfEnemies; i++) {
     es[i] = new Enemy(i);
-    var randX = Math.floor(Math.random()*amtOfCols);
-    var randY = Math.floor(Math.random()*amtOfRows);
-    while(checkForEnemy(randX, randY)) { // avoids enemies spawning on each other
-      var randX = Math.floor(Math.random()*amtOfCols);
-      var randY = Math.floor(Math.random()*amtOfRows);
-    };
-    es[i].place(randX, randY);
+    es[i].place(lvl.ecords[i][0], lvl.ecords[i][1]);
   }
   $('#msg').html('');
   setTimeout(function() {$('#playbutton').html('click to reset');}, 500);
@@ -371,31 +443,38 @@ var initGame = function() {
 }
 
 $(document).ready(function() {
+  initVars();
   gameActive = true;
   initsizes();
   drawGrid(amtOfCols, amtOfRows);
   updateVals();
   p = new Player();
-  p.place(1, 1);
+  p.place(lvl.pcords[0], lvl.pcords[1]);
+  //FOR RANDOM ENEMIES
+  // for(i = 0; i < amtOfEnemies; i++) {
+  //   es[i] = new Enemy(i);
+  //   var randX = Math.floor(Math.random()*amtOfCols);
+  //   var randY = Math.floor(Math.random()*amtOfRows);
+  //   while(checkForEnemy(randX, randY)) { // avoids enemies spawning on each other
+  //     var randX = Math.floor(Math.random()*amtOfCols);
+  //     var randY = Math.floor(Math.random()*amtOfRows);
+  //   };
+  //   es[i].place(randX, randY);
+  // }
   for(i = 0; i < amtOfEnemies; i++) {
     es[i] = new Enemy(i);
-    var randX = Math.floor(Math.random()*amtOfCols);
-    var randY = Math.floor(Math.random()*amtOfRows);
-    while(checkForEnemy(randX, randY)) { // avoids enemies spawning on each other
-      var randX = Math.floor(Math.random()*amtOfCols);
-      var randY = Math.floor(Math.random()*amtOfRows);
-    };
-    es[i].place(randX, randY);
+    es[i].place(lvl.ecords[i][0], lvl.ecords[i][1]);
   }
-  $('#msg').html('');
-  $('#playbutton').html('click to reset');
   for(i = 0; i < wc.length; i++) {
     ws[i] = new Wall(wc[i][0], wc[i][1], i);
   }
+
+  $('#msg').html('');
+  $('#playbutton').html('click to reset');
   $('#playbutton').click(function() {
     if(gameActive) {
       p.die();
-      setTimeout(function() {initGame()}, 100);
+      lvlselect = true;
     }
     else {
       initGame();
@@ -415,29 +494,21 @@ $(document).ready(function() {
       }
       switch(keyVal) {
         case 37: // left
-          p.left(p.speed);
+          p.left(speed);
           break;
 
         case 38: // up
-          p.up(p.speed);
+          p.up(speed);
           break;
 
         case 39: // right
-          p.right(p.speed);
+          p.right(speed);
           break;
 
         case 40: // down
-          p.down(p.speed);
+          p.down(speed);
           break;
       };
-      es.forEach(function(item) {
-        item.getDirection(p.x, p.y);
-        if(item.num === es.length - 1) {
-          if(checkForEnemy(p.x, p.y)) {
-            p.die();
-          }
-        }
-      });
     }
   });
 });
--- ui.js
+++ ui.js
@@ -1,5 +1,41 @@
 var state = 'down';
 $(document).ready(function() {
+  //Level grid
+
+  lvlCols = 10;
+  lvlRows = 9;
+
+  lvlH = $('#levels').height();
+  lvlW = $('#levels').width();
+
+  lvlMargin = Math.abs(((lvlW/lvlCols)-(lvlW/lvlCols+1)))
+
+  var lvlcounter = 0;
+
+  for(i = 0; i < lvlCols; i++) {
+    for(j = 0; j < lvlRows; j++) {
+      lvlcounter += 1;
+      $('#levels').append("<div id='lvl-outer"+lvlcounter+"' class='lvl-outer'><p class='lvl-outer-p'>"+lvlcounter+"</p></div>");
+    }
+    $('#levels').append("<br>");
+  }
+
+  $('.lvl-outer').css('height', lvlH/(lvlRows+1)+'px');
+  $('.lvl-outer').css('width', lvlW/(lvlCols+1)+'px');
+  $('.lvl-outer').css('margin-left', lvlMargin+'px');
+  $('.lvl-outer').css('margin-right', lvlMargin+'px');
+  $('#lvl-outer1').addClass('selected');
+
+  $('.lvl-outer').click(function() {
+    if(lvlselect) {
+      $('.lvl-outer').removeClass('selected');
+      $(this).addClass('selected');
+      lvl = lvls[Math.floor(($(this).attr('id'))[9])-1];
+    };
+  })
+
+  //Header
+
   $('#header-button').hover(function() {
     $('#header-h1').addClass('active');
     $('#header-button').addClass('active');
@@ -21,6 +57,12 @@ var toggleHeader = function() {
   else {
     $('#header-content').addClass('shown');
     $('#header-button').html("back to game &darr;");
+    if(gameActive) {
+      $('.lvl-outer').removeClass('selectable');
+    }
+    else {
+      $('.lvl-outer').addClass('selectable');
+    }
     state = 'up';
   }
 }
